5e2804b159a8656d1ef4a279b11809cf28888c50
Yass <yassineboulakhrif75@gmail.com>
2026-09-04 18:41:42 +0200

    config done

--- tests/stress-test.js
+++ tests/stress-test.js
@@ -3,16 +3,18 @@ import { check, sleep } from "k6";
 
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:3333';
 
+// J'utilise "stages" pour simuler une montée en charge progressive, de manière réaliste.
 export const options = {
   stages: [
-    { duration: '1m', target: 5 },
-    { duration: '1m', target: 10 },
-    { duration: '1m', target: 20 },
-    { duration: '1m', target: 50 },
     { duration: '1m', target: 100 },
+    { duration: '1m', target: 200 },
+    { duration: '1m', target: 400 },
+    { duration: '1m', target: 700 },
+    { duration: '1m', target: 1000 },
   ],
 };
 
+// Ici on peut définir des restrictions pour la pizza que l'utilisateur souhaiterait commander.
 export default function () {
   let restrictions = {
     maxCaloriesPerSlice: 500,
@@ -23,6 +25,7 @@ export default function () {
     minNumberOfToppings: 2
   };
 
+  // "res" va storer la réponse de l'API après avoir envoyé les restrictions pour la pizza.
   let res = http.post(`${BASE_URL}/api/pizza`, JSON.stringify(restrictions), {
     headers: {
       'Content-Type': 'application/json',
@@ -30,6 +33,7 @@ export default function () {
     },
   });
 
+  // check permet de vérifier que la réponse de l'API est bien un succès (status 200).
   check(res, { "status is 200": (res) => res.status === 200 });
   sleep(1);
 }
